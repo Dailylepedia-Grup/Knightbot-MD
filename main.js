c1405a5679a00ed24b56b64fe47a4560a93a9a8c
M Tri Hamdani <53250787+mhdtrihamdanii@users.noreply.github.com>
2025-06-25 13:51:26 +0700

    Update main.js
    Signed-off-by: M Tri Hamdani <53250787+mhdtrihamdanii@users.noreply.github.com>

--- main.js
+++ main.js
@@ -99,8 +99,8 @@ const videoCommand = require('./commands/video');
 // Global settings
 global.packname = settings.packname;
 global.author = settings.author;
-global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
-global.ytch = "Mr Unique Hacker";
+global.channelLink = "https://whatsapp.com/channel/0029VaZsNOG7z4kgDhIkmL0X";
+global.ytch = "M. Tri Hamdani";
 
 // Add this near the top of main.js with other global configurations
 const channelInfo = {
