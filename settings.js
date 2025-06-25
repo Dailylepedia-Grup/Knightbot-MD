862fff0e8bec8877f6ac43b8eb40b23bd3a3d7cb
M Tri Hamdani <53250787+mhdtrihamdanii@users.noreply.github.com>
2025-06-25 12:57:56 +0700

    Update settings.js
    Signed-off-by: M Tri Hamdani <53250787+mhdtrihamdanii@users.noreply.github.com>

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "PT. Dailylepedia Grup Sejahtera",
+  botOwner: 'M. Tri Hamdani', // Your name
+  ownerNumber: '6285183012695', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
